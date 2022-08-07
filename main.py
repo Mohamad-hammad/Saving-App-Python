fda251461d46251e6141c36d008a2191623a044e
Hammad <muhammadhammad1487@gmail.com>
2022-08-07 13:25:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+class Transaction:
+  def __init__(myself, title, type, amount):
+    myself.title = title
+    myself.type = type
+    myself.amount = amount
+
+class Month:
+  def __init__(myself, name, year):
+    myself.name = name
+    myself.year = year
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
